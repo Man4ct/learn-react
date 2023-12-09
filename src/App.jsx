49a4809b95073d3ecf27f220ae86aa5857912323
Fahmi Fahreza <fahmiiireza@gmail.com>
2023-12-09 13:57:46 +0100

    implement basin routing and css styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,44 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom"
-
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  HomeLayout,
+  Landing,
+  Register,
+  Error,
+  Login,
+  DashboardLayout,
+} from "./pages";
 const router = createBrowserRouter([
   {
-   path: "/",
-   element: <h1>Home page</h1>
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error/>,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "dashboard",
+        element: <DashboardLayout />,
+      },
+    ],
   },
   {
-    path: "/about",
-    element: <h1>About page</h1>
-   }
-])
+    path: "/error",
+    element: <Error />,
+  },
+]);
 
 const App = () => {
-  return <RouterProvider router={router} />
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,6 +1,12 @@
+import { Link, useRouteError } from "react-router-dom"
+
 const Error = () => {
+  const error = useRouteError()
+  console.log(error)
   return (
-    <div>Error</div>
+    <div><h1>error</h1>
+    <Link to='/'>back home</Link>
+    </div>
   )
 }
 export default Error
