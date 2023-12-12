7050139f475390c57c7497cff9b3c62f9444c091
Fahmi Fahreza <fahmiiireza@gmail.com>
2023-12-12 19:37:44 +0100

    Implement custom image for error 404 and added form for register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,29 @@
 import { Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { Logo, FormRow } from '../components';
 
 const Register = () => {
   return (
-    <div><h1>Register</h1>
-        <Link to='/login'>Login Page</Link>
+    <Wrapper>
+      <form className='form'>
+        <Logo />
+        <FormRow type='text' name='name' defaultValue='john'/>
+        <FormRow type='text' name='name' defaultValue='john'/>
+        <FormRow type='text' name='name' defaultValue='john'/>
+        <FormRow type='text' name='name' defaultValue='john'/>
+        <FormRow type='text' name='name' defaultValue='john'/>
 
-    </div>
-
-  )
-}
-export default Register
+        <h4>Register</h4>
+        
+        <button className='btn btn-block'>Submit</button>
+        <p>
+          Already a member?
+          <Link to='/login' className='member-btn'>
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
+  );
+};
+export default Register;
