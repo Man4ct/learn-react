187ef879f481061d75faa50eb10d0e1a7888bf61
Fahmi Fahreza <fahmiiireza@gmail.com>
2023-12-18 14:01:11 +0100

    added navbar as component

--- src/components/SmallSideBar.jsx
+++ src/components/SmallSideBar.jsx
@@ -4,6 +4,7 @@ import { useDashboardContext } from '../pages/DashboardLayout';
 import Logo from './Logo';
 import links from '../utils/links';
 import { NavLink } from 'react-router-dom';
+import NavLinks from './NavLinks';
 const SmallSideBar = () => {
   const { showSidebar, toggleSidebar } = useDashboardContext();
   return (
@@ -20,23 +21,7 @@ const SmallSideBar = () => {
           <header>
             <Logo />
           </header>
-          <div className='nav-links'>
-            {links.map((link) => {
-              const { text, path, icon } = link;
-              return (
-                <NavLink
-                  to={path}
-                  key={text}
-                  className='nav-link'
-                  onClick={toggleSidebar}
-                  end
-                >
-                  <span className='icon'>{icon}</span>
-                  {text}
-                </NavLink>
-              );
-            })}
-          </div>
+          <NavLinks />
         </div>
       </div>
     </Wrapper>
