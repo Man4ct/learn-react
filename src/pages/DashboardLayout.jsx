c49e315e38deff5c649da8a3df8f37a053685a5b
Fahmi Fahreza <fahmiiireza@gmail.com>
2023-12-19 10:55:23 +0100

    bugfix dark theme toggle

--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -2,18 +2,19 @@ import { Outlet } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/Dashboard';
 import { BigSidebar, Navbar, SmallSidebar } from '../components';
 import { createContext, useContext, useState } from 'react';
-
+import { checkDefaultTheme } from '../App';
 const DashboardContext = createContext();
 
-const DashboardLayout = () => {
+const DashboardLayout = (isDarkThemeEnabled) => {
   const user = { name: 'john' };
   const [showSidebar, setShowSidebar] = useState(false);
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(checkDefaultTheme);
 
   const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme
     setIsDarkTheme(newDarkTheme)
     document.body.classList.toggle('dark-theme', newDarkTheme)
+    localStorage.setItem('darkTheme', newDarkTheme)
     console.log('toggle dark theme');
   };
 
