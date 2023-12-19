1ca5b1d7297959f4d79c401d69a3817d549417e6
Fahmi Fahreza <fahmiiireza@gmail.com>
2023-12-19 10:08:01 +0100

    implement darkTheme toggle

--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -11,6 +11,9 @@ const DashboardLayout = () => {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
 
   const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme
+    setIsDarkTheme(newDarkTheme)
+    document.body.classList.toggle('dark-theme', newDarkTheme)
     console.log('toggle dark theme');
   };
 
